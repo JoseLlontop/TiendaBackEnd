5f36b1f4992d4df50828241fcfbfb6ae3bedd64e
JoseLlontop <josellontop100@gmail.com>
2024-05-23 11:03:07 -0300

    Se agrego las configuracines necesarias para deplejar el BackEnd

--- src/main/java/com/trabajoFinal/trabajoFinal/config/CorsConfig.java
+++ src/main/java/com/trabajoFinal/trabajoFinal/config/CorsConfig.java
@@ -7,10 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    private final String dominioLocal = "http://localhost:5173";
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
--- src/main/java/com/trabajoFinal/trabajoFinal/config/StorageConfig.java
+++ src/main/java/com/trabajoFinal/trabajoFinal/config/StorageConfig.java
@@ -13,13 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StorageConfig {
 
-    @Value("${aws.accessKey}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${aws.secretKey}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${aws.region}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
